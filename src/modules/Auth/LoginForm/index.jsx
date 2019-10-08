69906df1d96c09b0f3eccd9c956471a03c5d9caa
ValentinJeudy <valentin.jeudy@gmail.com>
2019-10-08 14:15:44 +0200

    add field validation when signin in

--- src/modules/Auth/LoginForm/index.jsx
+++ src/modules/Auth/LoginForm/index.jsx
@@ -2,9 +2,9 @@ import React, { useState } from 'react'
 import { TextField, Button } from '@material-ui/core'
 
 // styles
-const textFieldStyle = {
-	width: '100%'
-}
+// const textFieldStyle = {
+// 	width: '100%'
+// }
 
 const buttonStyle = {
 	marginTop: '1rem',
@@ -13,35 +13,90 @@ const buttonStyle = {
 
 const LoginForm = (props) => {
 	const [form, setForm] = useState({
-		pseudo: '',
-		password: ''
+		username: {
+			field: '',
+			error : false,
+			msg: ''
+		},
+		password: {
+			field: '',
+			error: false,
+			msg: ''
+		}
 	})
 
-	const logUser = () => {
+	const authUser = (e) => {
 
+		const isFormValid = validateFields()
+
+		console.log('isFormValid ===> ', isFormValid)
+	}
+
+	const handleChange = (name) => ({ target: { value } }) => {
+		setForm({
+			...form,
+			[name]: {
+				field: value,
+				error: false,
+				msg: ''
+			}
+		})
+	}
+
+	const validateFields = () => {
+		let isValid = true
+
+		const newForm = Object.keys(form).reduce((acc, name)=> {
+			if (!form[name].field.length) {
+				isValid = false
+				return	{
+					...acc,
+					[name]: {
+						field: form[name].field,
+						error: true,
+						msg: 'This field cannot be empty'
+					}
+				}
+			} else {
+				return {
+					...acc,
+					[name]: form[name]
+				}
+			}
+		}, {})
+
+		setForm(newForm)
+
+		return isValid
 	}
 
 	return(
 		<form>
 			<TextField
-				style={textFieldStyle}
-				label="Name"
+				label="Username"
 				type="text"
-				autoComplete="current-password"
-				margin="normal"
+				required={true}
+				error={form.username.error}
+				helperText={form.username.msg}
+				value={form.username.field}
+				onChange={handleChange('username')}
+				fullWidth={true}
 			/>
 			<br/>
 			<TextField
-				style={textFieldStyle}
 				label="Password"
 				type="password"
-				autoComplete="current-password"
-				margin="normal"
+				required={true}
+				error={form.password.error}
+				helperText={form.password.msg}
+				value={form.password.field}
+				onChange={handleChange('password')}
+				fullWidth={true}
 			/>
 			<Button
 				variant="contained"
 				color="secondary"
-				onClick={}
+				onClick={authUser}
 				style={buttonStyle}>
 				Login
 			</Button>
