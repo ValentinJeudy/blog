5b07bb68dbd16eb87f428fae5fef7c0b5e5086aa
ValentinJeudy <valentin.jeudy@gmail.com>
2019-10-09 20:08:43 +0200

    add network file to make requests

---
+++ src/lib/network.js
@@ -0,0 +1,80 @@
+// import axios from 'axios'
+import { apiConfig } from './config.js'
+import axios from 'axios'
+
+export const get = async (route, params) => {
+	const token = getToken()
+	const headers = {
+		...token ? { 'Authorization': `Bearer ${token}` } : {}
+	}
+
+	try {
+		return await axios({
+			method: 'get',
+			url: `${apiConfig.url}/${route}`,
+			headers,
+			...params ? { params } : {}
+		})
+	} catch (err) {
+		console.error(
+			`Error during get request on API:
+      ${err}`
+		)
+	}
+}
+
+export const put = async (route, data) => {
+	const token = getToken()
+	const headers = {
+		...token ? { 'Authorization': `Bearer ${token}` } : {}
+	}
+
+	try {
+		return await axios({
+			method: 'put',
+			url: `${apiConfig.url}/${route}`,
+			headers,
+			data
+		})
+	} catch (err) {
+		console.error(
+			`Error trying to put request on API:
+      ${err}`
+		)
+	}
+}
+
+export const post = async (route, data = {}) => {
+	const token = getToken()
+
+	console.log('token in post ===> ', token)
+	const headers = {
+		...token ? { 'Authorization': `Bearer ${token}` } : {}
+	}
+
+	console.log('`${apiConfig.url}/${route}` ===> ', `${apiConfig.url}/${route}`)
+
+	try {
+		return await axios({
+			method: 'post',
+			url: `${apiConfig.url}/${route}`,
+			data,
+			headers
+		})
+	} catch (err) {
+		console.error(
+			`Error during post request on API:
+      ${err.response}`
+		)
+		return err.response
+	}
+}
+
+const getToken = () => {
+	const cookies = document.cookie.split(';')
+
+	const token = cookies
+		.find((token) => token.split('=')[0].trim() === 'blog-token')
+
+	return token ? token.split('=').splice(1).join('=') : ''
+}
