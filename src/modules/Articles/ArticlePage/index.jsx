91be15d409a577cfedeb3ce94f44e12ef639bd5a
ValentinJeudy <valentin.jeudy@gmail.com>
2019-10-14 23:15:36 +0200

    add article upload

--- src/modules/Articles/ArticlePage/index.jsx
+++ src/modules/Articles/ArticlePage/index.jsx
@@ -2,146 +2,162 @@ import React, { useContext, useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 import { makeStyles } from '@material-ui/core/styles'
 
-import { get, put } from '../../../lib/network'
-import { UserContext } from '../../Common/UserContext'
+import { get, put } from 'src/lib/network'
+import { UserContext } from 'src/modules/Common/UserContext'
 import EditArticle from '../EditArticle'
 import Article from '../Article'
-import SnackBar from '../../Common/SnackBar'
+import SnackBar from 'src/modules/Common/SnackBar'
 
 // Styles
 const useStyles = makeStyles({
-	container: {
-		display: 'flex',
-		justifyContent: 'space-between',
-	},
-	title: {
-		marginBottom: '1.5rem',
-		border: 'none'
-	},
-	block: {
-		width: '45%',
-		alignItems: 'top',
-		minHeight: '25rem'
-	},
-	editor: {
-		minHeight: '25rem'
-	},
-	buttonsContainer: {
-		display: 'flex',
-		justifyContent: 'flex-end',
-		marginBottom: '1.5rem'
-	},
-	buttons: {
-		width: '15rem',
-		textAlign: 'right'
-	},
-	button: {
-		marginLeft: '1.5rem'
-	}
+  container: {
+    display: 'flex',
+    justifyContent: 'space-between'
+  },
+  title: {
+    marginBottom: '1.5rem',
+    border: 'none'
+  },
+  block: {
+    width: '45%',
+    alignItems: 'top',
+    minHeight: '25rem'
+  },
+  editor: {
+    minHeight: '25rem'
+  },
+  buttonsContainer: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    marginBottom: '1.5rem'
+  },
+  buttons: {
+    width: '15rem',
+    textAlign: 'right'
+  },
+  button: {
+    marginLeft: '1.5rem'
+  }
 })
 
 const ArticlePage = (props) => {
-	const classes = useStyles()
-	const [user, setUser] = useContext(UserContext)
-	const [selectedFile, setSelectedFile] = useState(null)
-	const [snackbar, setSnackbar] = useState({
-		open: false,
-		message: '',
-	})
-	const [article, setArticle] = useState({
-		_id: '',
-		title: '',
-		imgPath: '',
-		content: '',
-		tags: []
-	})
+  const classes = useStyles()
+  const [user, setUser] = useContext(UserContext)
+  const [selectedFile, setSelectedFile] = useState(null)
+  const [snackbar, setSnackbar] = useState({
+    open: false,
+    message: ''
+  })
+  const [article, setArticle] = useState({
+    _id: '',
+    title: '',
+    imgName: '',
+    content: '',
+    tags: []
+  })
 
-	const validUserToken = async () => {
-		const res = await get('users/verify')
+  const saveArticle = async () => {
+    if (selectedFile) {
+      const fd = new FormData()
+      fd.append('image', selectedFile, selectedFile.name)
+      fd.append('article', JSON.stringify(article))
+      const headers = { 'Content-Type': 'multipart/form-data' }
 
-		if (res.status === 200) {
-			setUser({
-				logged: true,
-				token: res.data.token
-			})
-		} else {
-			setUser({
-				logged: false,
-				token: ''
-			})
-		}
-	}
+      const res = await put('articles', fd, headers)
 
-	const setProperArticle = () => {
-		const locationState = props.history.location.state
+      if (res.data.success) {
+        setArticle({
+          ...res.data.article
+        })
+        setSelectedFile(null)
+        setSnackbar({
+          open: true,
+          message: 'Document has been saved and file uploaded'
+        })
+      } else {
+        console.log('res.data ===> ', res.data)
+        setSnackbar({
+          open: true,
+          message: `Errors: ${res.data.errors}`
+        })
+      }
+    } else {
+      const res = await put('articles', { article })
 
-		if (props.article) {
-			setArticle({
-				...props.article
-			})
-		} else if (locationState && locationState.article) {
-			const data = {
-				...locationState.article
-			}
+      if (res.status === 200) {
+        setSnackbar({
+          open: true,
+          message: 'Document has been saved !!'
+        })
+      }
+    }
+  }
 
-			setArticle(data)
-		}
-	}
+  // Executed when page rendered
+  useEffect(() => {
+    const validUserToken = async () => {
+      const res = await get('users/verify')
 
-	const saveArticle = async () => {
-		if (selectedFile) {
-			const fd = new FormData()
-			fd.append('image', selectedFile, selectedFile.name)
-			fd.append('article', JSON.stringify(article))
-			const headers = {'Content-Type': 'multipart/form-data' }
+      if (res.status === 200) {
+        setUser({
+          logged: true,
+          token: res.data.token
+        })
+      } else {
+        setUser({
+          logged: false,
+          token: ''
+        })
+      }
+    }
 
-			const res = await put('articles', fd, headers)
+    const setProperArticle = () => {
+      const locationState = props.history.location.state
 
-			console.log('res ===> ', res)
-		} else {
-			const res = await put('articles', { article })
+      if (props.article) {
+        setArticle({
+          ...props.article
+        })
+      } else if (locationState && locationState.article) {
+        const data = {
+          ...locationState.article
+        }
 
-			if (res.status === 200) {
-				setSnackbar({
-					open: true,
-					message: 'Document has been saved !!'
-				})
-			}
-		}
-	}
+        setArticle(data)
+      }
+    }
 
-	useEffect(() => {
-		const setup = async () => {
-			await validUserToken()
-			await setProperArticle()
-		}
+    const setup = async () => {
+      await validUserToken()
+      setProperArticle()
+    }
 
-		setup()
-	}, [])
+    setup()
+  }, [props.article, props.history.location.state])
 
-	return(
-		<div className="container">
-			<h2>Article page</h2>
-			<div className={classes.container}>
-				{ user.logged
-					&& <EditArticle
-						article={article}
-						setArticle={setArticle}
-						saveArticle={saveArticle}
-						selectedFile={selectedFile}
-						setSelectedFile={setSelectedFile}
-						classes={classes}/> }
-				<Article article={article} classes={classes} />
-				{ snackbar.open && <SnackBar snackbar={snackbar} setSnackbar={setSnackbar} /> }
-			</div>
-		</div>
-	)
+  return (
+    <div className='container'>
+      <h2>Article page</h2>
+      <div className={classes.container}>
+        {user.logged &&
+          <EditArticle
+            article={article}
+            setArticle={setArticle}
+            saveArticle={saveArticle}
+            selectedFile={selectedFile}
+            setSelectedFile={setSelectedFile}
+            classes={classes}
+          />}
+        <Article article={article} classes={classes} />
+        {snackbar.open && <SnackBar snackbar={snackbar} setSnackbar={setSnackbar} />}
+      </div>
+    </div>
+  )
 }
 
 ArticlePage.propTypes = {
-	article: PropTypes.object,
-	history: PropTypes.object
+  article: PropTypes.object,
+  history: PropTypes.object
 }
-
 
 export default ArticlePage
--- src/modules/Articles/EditArticle/index.jsx
+++ src/modules/Articles/EditArticle/index.jsx
@@ -9,114 +9,114 @@ import './EditArticle.scss'
 
 // Quill Editor Options
 const toolbarOptions = [
-	[{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-	['blockquote', 'code-block'],
-	['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+  ['blockquote', 'code-block'],
+  ['bold', 'italic', 'underline', 'strike'], // toggled buttons
 
-	[{ 'list': 'ordered'}, { 'list': 'bullet' }],
-	// [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
-	[{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
-	// [{ 'direction': 'rtl' }],                         // text direction
+  [{ list: 'ordered' }, { list: 'bullet' }],
+  // [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+  [{ indent: '-1' }, { indent: '+1' }], // outdent/indent
+  // [{ 'direction': 'rtl' }],                         // text direction
 
-	// [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+  // [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
 
-	// [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
-	[{ 'align': [] }],
+  // [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+  [{ align: [] }]
 
-	// ['clean']
+  // ['clean']
 ]
 
 const editorModules = {
-	toolbar: toolbarOptions
+  toolbar: toolbarOptions
 }
 
 const EditArticle = ({ article, setArticle, saveArticle, selectedFile, setSelectedFile, classes }) => {
-	// const [selectedFile, setSelectedFile] = useState({})
+  // const [selectedFile, setSelectedFile] = useState({})
 
-	const setContent= (updatedContent, delta, source) => {
-		if (source === 'user') {
-			const data = {
-				...article,
-				content: updatedContent,
-			}
-			setArticle(data)
-		}
-	}
+  const setContent = (updatedContent, delta, source) => {
+    if (source === 'user') {
+      const data = {
+        ...article,
+        content: updatedContent
+      }
+      setArticle(data)
+    }
+  }
 
-	const setTitle = (e) => {
-		setArticle({
-			...article,
-			title: e.target.value,
-		})
-	}
+  const setTitle = (e) => {
+    setArticle({
+      ...article,
+      title: e.target.value
+    })
+  }
 
-	const fileUploadHandler = (e) => {
-		if (e.target.files && e.target.files.length) {
-			setSelectedFile(e.target.files[0])
-		}
-	}
+  const fileUploadHandler = (e) => {
+    if (e.target.files && e.target.files.length) {
+      setSelectedFile(e.target.files[0])
+      e.target.value = ''
+    }
+  }
 
-	return(
-		<div className={classes.block}>
-			<TextField
-				className={classes.title}
-				label='Title'
-				type='text'
-				value={article.title}
-				onChange={setTitle}
-				fullWidth={true}
-				variant='outlined'
-			/>
-			<div className={classes.buttonsContainer}>
-				<div>
-					<span>
-						{ selectedFile && selectedFile.name }
-					</span>
-				</div>
-				<div className={classes.buttons}>
-					<input
-						style={{display: 'none'}}
-						accept='image/*'
-						id='raised-button-file'
-						type='file'
-						onChange={fileUploadHandler}
-					/>
-
-					<label htmlFor='raised-button-file'>
-						<Button
-							variant='contained'
-							color='secondary'
-							component='span'>
-          Upload
-						</Button>
-					</label>
-					<Button
-						className={classes.button}
-						variant='contained'
-						color='secondary'
-						onClick={saveArticle}
-					>
-					Save
-					</Button>
-				</div>
-			</div>
-			<div>
-				<ReactQuill
-					modules={editorModules}
-					value={article.content}
-					onChange={setContent} />
-			</div>
-		</div>
-	)
+  return (
+    <div className={classes.block}>
+      <TextField
+        className={classes.title}
+        label='Title'
+        type='text'
+        value={article.title}
+        onChange={setTitle}
+        fullWidth
+        variant='outlined'
+      />
+      <div className={classes.buttonsContainer}>
+        <div>
+          <span>
+            {selectedFile && selectedFile.name}
+          </span>
+        </div>
+        <div className={classes.buttons}>
+          <input
+            style={{ display: 'none' }}
+            accept='image/*'
+            id='raised-button-file'
+            type='file'
+            onChange={fileUploadHandler}
+          />
+          <label htmlFor='raised-button-file'>
+            <Button
+              variant='contained'
+              color='secondary'
+              component='span'
+            > Upload
+            </Button>
+          </label>
+          <Button
+            className={classes.button}
+            variant='contained'
+            color='secondary'
+            onClick={saveArticle}
+          > Save
+          </Button>
+        </div>
+      </div>
+      <div>
+        <ReactQuill
+          modules={editorModules}
+          value={article.content}
+          onChange={setContent}
+        />
+      </div>
+    </div>
+  )
 }
 
 EditArticle.propTypes = {
-	article: PropTypes.object,
-	setArticle: PropTypes.func,
-	saveArticle: PropTypes.func,
-	selectedFile: PropTypes.object,
-	setSelectedFile: PropTypes.func,
-	classes: PropTypes.object,
+  article: PropTypes.object,
+  setArticle: PropTypes.func,
+  saveArticle: PropTypes.func,
+  selectedFile: PropTypes.object,
+  setSelectedFile: PropTypes.func,
+  classes: PropTypes.object
 }
 
 export default EditArticle
