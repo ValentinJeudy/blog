89d234273808ed448f26c9f43fd02435365a39ed
ValentinJeudy <valentin.jeudy@gmail.com>
2019-10-14 23:15:07 +0200

    modified eslintrc to use standard

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -1,47 +1,47 @@
 module.exports = {
   env: {
       browser: true,
-      es6: true
+    //   es6: true
   },
   extends: [
-      'eslint:recommended',
-      'plugin:react/recommended',
-      // 'standard',
-      // 'standard-react'
-      // 'plugin:@typescript-eslint/eslint-recommended'
+    //   'eslint:recommended',
+    //   'plugin:react/recommended',
+    'standard-react',
+    'standard',
+    // 'plugin:@typescript-eslint/eslint-recommended'
   ],
-  globals: {
-      Atomics: 'readonly',
-      SharedArrayBuffer: 'readonly'
-  },
-  // parser: 'babel-eslint',
-  parserOptions: {
-      ecmaFeatures: {
-          jsx: true
-      },
-      ecmaVersion: 2018,
-      sourceType: 'module'
-  },
-  plugins: [
-      'react',
+//   globals: {
+//       Atomics: 'readonly',
+//       SharedArrayBuffer: 'readonly'
+//   },
+//   parser: 'babel-eslint',
+//   parserOptions: {
+//       ecmaFeatures: {
+//           jsx: true
+//       },
+//       ecmaVersion: 2018,
+//       sourceType: 'module'
+//   },
+//   plugins: [
+    //   'react',
       // '@typescript-eslint'
-  ],
-  rules: {
-      indent: [
-          'error',
-          'tab'
-      ],
-      'linebreak-style': [
-          'error',
-          'unix'
-      ],
-      quotes: [
-          'error',
-          'single'
-      ],
-      semi: [
-          'error',
-          'never'
-      ]
-  }
+//   ],
+//   rules: {
+//       indent: [
+//           'error',
+//           'tab'
+//       ],
+//       'linebreak-style': [
+//           'error',
+//           'unix'
+//       ],
+//       quotes: [
+//           'error',
+//           'single'
+//       ],
+//       semi: [
+//           'error',
+//           'never'
+//       ]
+//   }
 };
