e6dbcf60822160fbc0ad07ae331bc117f340b12c
ValentinJeudy <valentin.jeudy@gmail.com>
2019-10-14 13:40:02 +0200

    creating article before redirect

--- src/modules/Articles/AddArticle/index.jsx
+++ src/modules/Articles/AddArticle/index.jsx
@@ -1,21 +1,41 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import {
 	Card,
 	CardHeader,
 	CardActions,
 	Button } from '@material-ui/core'
+import { post } from '../../../lib/network'
 
-const AddArticle = ({ classes }) => {
+
+const AddArticle = (props) => {
+	const history = useHistory()
+
+	const createArticle = async () => {
+		const data = {
+			title: '',
+			content: '',
+			tags: []
+		}
+		const res = await post('articles', {article: data})
+
+		if (res.data.success) {
+			const article = res.data.article
+
+			history.push({
+				pathname: `/articles/${article._id}`,
+				state: { article }
+			})
+		}
+	}
+
 	return(
-		<li className={classes.li}>
+		<li className={props.classes.li}>
 			<Card>
 				<CardHeader title="New article" />
 				<CardActions>
-					<Link to='/articles/new'>
-						<Button>Create</Button>
-					</Link>
+					<Button onClick={createArticle}>Create</Button>
 				</CardActions>
 			</Card>
 		</li>
@@ -23,7 +43,8 @@ const AddArticle = ({ classes }) => {
 }
 
 AddArticle.propTypes = {
-	classes: PropTypes.object
+	classes: PropTypes.object,
+	history: PropTypes.object
 }
 
 
