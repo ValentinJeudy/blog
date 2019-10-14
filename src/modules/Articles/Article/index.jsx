bd8d2a36adfe7bec5ed9903c37768ac16fee90a9
ValentinJeudy <valentin.jeudy@gmail.com>
2019-10-14 23:16:03 +0200

    trying to import file dynamically

--- src/modules/Articles/Article/index.jsx
+++ src/modules/Articles/Article/index.jsx
@@ -1,20 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
+// import image from '../../../uploads/pool.jpg'
 
 const Article = ({ article, classes }) => {
+  const [imageStyle, setImageStyle] = useState(null)
 
-	return(
-		<div className={classes.block}>
-			<h3>{ article.title || 'No Title' }</h3>
-			<div dangerouslySetInnerHTML={{ __html: article.content }}></div>
-		</div>
-	)
+  const imgBlockStyle = {
+    // backgroundImage: `url("../../../uploads/${article.imgName}")`,
+    height: '5rem'
+  }
+
+  useEffect(() => {
+    const importImage = async () => {
+      const { default: importedImage } = await import(`src/uploads/${article.imgName}`)
+
+      console.log('importedImage ===> ', importedImage)
+
+      setImageStyle({
+        backgroundImage: `url("${importedImage}")`,
+        height: '5rem'
+      })
+    }
+
+    importImage()
+  }, [article.imgName, setImageStyle])
+
+  return (
+    <div className={classes.block}>
+      <h3>{article.title || 'No Title'}</h3>
+      <div style={imageStyle} />
+      {article.imgName}
+      {/* {article.imgName && <img src={import(`src/uploads/${article.imgName}`)} />} */}
+      <div dangerouslySetInnerHTML={{ __html: article.content }} />
+    </div>
+  )
 }
 
 Article.propTypes = {
-	article: PropTypes.object,
-	classes: PropTypes.object
+  article: PropTypes.object,
+  classes: PropTypes.object
 }
-
 
 export default Article
