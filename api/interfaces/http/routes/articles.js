7ef9ee733e0dd6ce8abde049151c96f3a8d5db88
ValentinJeudy <valentin.jeudy@gmail.com>
2019-10-15 16:53:45 +0200

    store uploads in folder at root

--- api/interfaces/http/routes/articles.js
+++ api/interfaces/http/routes/articles.js
@@ -3,7 +3,7 @@ const multer = require('multer')
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'src/uploads')
+    cb(null, 'uploads')
   },
   filename: (req, file, cb) => {
     // const fileName = `${new Date().toISOString()}-${fie.originalname}`
